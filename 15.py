560e7ffca1712753d3a94a34cea00cf30b8ab63a
djchill1 <djchill10@googlemail.com>
2020-10-12 22:42:50 +0100

    day 15 complete

--- 15.py
+++ 15.py
@@ -8,14 +8,15 @@ with open('15_input.txt') as fp:
 with open('15_test.txt') as fp:
     testData = fp.read().splitlines()
 
-cookieCapacity = 100
-names = []
-capacaties = []
-durabilities = []
-flavours = []
-textures = []
 
 def setup(data):
+    cookieCapacity = 100
+    names = []
+    capacaties = []
+    durabilities = []
+    flavours = []
+    textures = []
+    calories = []
     for line in data:
         parsed = re.split(" ", line, 15)
         print(parsed)
@@ -24,11 +25,58 @@ def setup(data):
         durabilities.append(parsed[4].split(',')[0])
         flavours.append(parsed[6].split(',')[0])
         textures.append(parsed[8].split(',')[0])
-    return names, capacaties, durabilities, flavours, textures
-
-def getScore():
-
+        calories.append(parsed[10].split(',')[0])
+    return names, capacaties, durabilities, flavours, textures, calories
 
 
+def getScore(percs, capacaties, durabilities, flavours, textures, calories):
+    max = len(percs)
+    cap, dur, fla, tex, cal = 0, 0, 0, 0, 0
+    for i in range(0, max):
+        # print('index', i)
+        cap += percs[i] * int(capacaties[i])
+        dur += percs[i] * int(durabilities[i])
+        fla += percs[i] * int(flavours[i])
+        tex += percs[i] * int(textures[i])
+        cal += percs[i] * int(calories[i])
+    if cap < 0:
+        cap = 0
+    elif dur < 0:
+        dur = 0
+    elif fla < 0:
+        fla = 0
+    # print(percs, cap, dur, fla, tex, cap * dur * fla * tex)
+    return cap * dur * fla * tex, cal
 
-print(setup(testData))
+
+def generate_weights(n):
+    for t in itertools.combinations_with_replacement(range(100), n):
+        if sum(t) == 100:
+            yield from itertools.permutations(t)
+
+
+# print(setup(testData))
+# print(getScore([44, 56], ['-1', '2'], ['-2', '3'], ['6', '-2'], ['3', '-1']))
+
+
+def getMax(data, calorieGoal=None):
+    names, capacaties, durabilities, flavours, textures, calories = setup(data)
+    numbOf = len(names)
+    # print('numOf' ,numbOf, names)
+    weights = generate_weights(numbOf)
+    maxScore = 0
+    for ratio in weights:
+        # print('ratio is', ratio)
+        score, cals = getScore(ratio, capacaties, durabilities, flavours, textures, calories)
+        if score > maxScore:
+            if calorieGoal is not None:
+                if cals == calorieGoal:
+                    maxScore = score
+            else:
+                maxScore = score
+    return maxScore
+
+
+print('max is', getMax(testData))
+print('max is', getMax(data))
+print('max is', getMax(data, 500))
