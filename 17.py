1d3ce04b1e2a7c58e41a101b2bb1dd27032ab793
djchill1 <djchill10@googlemail.com>
2020-10-14 20:01:43 +0100

    refactor efficiency + day 17 solution

--- 17.py
+++ 17.py
@@ -1,5 +1,4 @@
 import itertools
-import math
 
 with open('17_input.txt') as fp:
     data = fp.read().splitlines()
@@ -8,35 +7,16 @@ containersInts = []
 for i in data:
     containersInts.append(int(i))
 
+# Part 1 - Sum the number of all combinations that hold 150 L.
+total = 0
+for i in range(1, len(containersInts) + 1):
+    total += len([x for x in itertools.combinations(containersInts, i) if sum(x) == 150])
+print(total)
 
-def generate_permutations(containers, total):
-    for i in range(len(containers)):
-        for t in itertools.combinations(containers, i):
-            if sum(t) == total:
-                yield from itertools.permutations(t)
-
-
-perms = generate_permutations(containersInts, 150)
-lengths = []
-counts = []
-for perm in perms:
-    lengths.append(len(perm))
-    # print(sorted(perm))
-for i in range(len(lengths)):
-    # print(i, 'count', int(lengths.count(i)/math.factorial(i)))
-    counts.append(int(lengths.count(i)/math.factorial(i)))
-print(sum(counts))
-
-# containersInts = [20, 15, 10, 5, 5]
-# perms = generate_permutations(containersInts, 25)
-# lengths = []
-# counts = []
-# for perm in perms:
-#     lengths.append(len(perm))
-#     print(sorted(perm))
-# for i in range(len(lengths)):
-#     print(i, 'count', int(lengths.count(i)/math.factorial(i)))
-#     counts.append(int(lengths.count(i)/math.factorial(i)))
-# print(sum(counts))
-
-
+# Part 2 - Break after the smallest number
+total = 0
+for i in range(1, len(containersInts) + 1):
+    total += len([x for x in itertools.combinations(containersInts, i) if sum(x) == 150])
+    if total > 0:
+        break
+print(total)
